6db22ca2deeb04c80f4cd288f0202c53ae068eeb
Alberto Fernandez <infjaf@gmail.com>
2024-09-28 16:29:55 +0200

    Add ECDSA support to PdfPKCS7

--- openpdf/src/main/java/com/lowagie/text/pdf/PdfPKCS7.java
+++ openpdf/src/main/java/com/lowagie/text/pdf/PdfPKCS7.java
@@ -155,6 +155,7 @@ public class PdfPKCS7 {
     private static final String ID_PKCS7_SIGNED_DATA = "1.2.840.113549.1.7.2";
     private static final String ID_RSA = "1.2.840.113549.1.1.1";
     private static final String ID_DSA = "1.2.840.10040.4.1";
+    private static final String ID_ECDSA = "1.2.840.10045.2.1";
     private static final String ID_CONTENT_TYPE = "1.2.840.113549.1.9.3";
     private static final String ID_MESSAGE_DIGEST = "1.2.840.113549.1.9.4";
     private static final String ID_SIGNING_TIME = "1.2.840.113549.1.9.5";
@@ -227,6 +228,7 @@ public class PdfPKCS7 {
         algorithmNames.put("1.3.36.3.3.1.3", "RSA");
         algorithmNames.put("1.3.36.3.3.1.2", "RSA");
         algorithmNames.put("1.3.36.3.3.1.4", "RSA");
+        algorithmNames.put(ID_ECDSA, "ECDSA");
 
         allowedDigests.put("MD5", "1.2.840.113549.2.5");
         allowedDigests.put("MD2", "1.2.840.113549.2.2");
@@ -604,7 +606,10 @@ public class PdfPKCS7 {
                 digestEncryptionAlgorithm = ID_RSA;
             } else if (digestEncryptionAlgorithm.equals("DSA")) {
                 digestEncryptionAlgorithm = ID_DSA;
-            } else {
+            } else if (digestEncryptionAlgorithm.equals("EC")) {
+                digestEncryptionAlgorithm = ID_ECDSA;
+            }
+            else {
                 throw new NoSuchAlgorithmException(
                         MessageLocalization.getComposedMessage("unknown.key.algorithm.1",
                                 digestEncryptionAlgorithm));
@@ -1190,10 +1195,13 @@ public class PdfPKCS7 {
                 this.digestEncryptionAlgorithm = ID_RSA;
             } else if (digestEncryptionAlgorithm.equals("DSA")) {
                 this.digestEncryptionAlgorithm = ID_DSA;
-            } else
+            } else if (digestEncryptionAlgorithm.equals("EC")) {
+                digestEncryptionAlgorithm = ID_ECDSA;
+            } else {
                 throw new ExceptionConverter(new NoSuchAlgorithmException(
                         MessageLocalization.getComposedMessage("unknown.key.algorithm.1",
                                 digestEncryptionAlgorithm)));
+            }
         }
     }
 
